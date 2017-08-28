260005a65f74dfc34aafa7dfe7d113430f665987
zhuanli555 <1847633840@qq.com>
2017-08-28 17:38:44 +0800

    orm_py add

---
+++ www/orm.py
@@ -0,0 +1,23 @@
+import aiomysql
+import asyncio
+import logging
+
+#线程池
+def create_pool(loop,**kw):
+    logging.info("create database connection pool...")
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host = kw.get('host','localhost'),
+        port = kw.get('port',3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset','utf-8'),
+        autocommit = kw.get('autocommit',True),
+        maxsize = kw.get('maxsize',10),
+        minsize = kw.get('minsize',1),
+        loop = loop
+    )
+#封装select
+def select(sql,args,size=None):
+    
